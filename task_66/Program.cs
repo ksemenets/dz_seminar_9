e8932af91ab79497bf5745ac06c56595db533ad7
Konstantin <ksemenec@mail.ru>
2023-02-06 21:24:38 +0500

    task66

---
+++ task_66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N.
+// Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int GetSumma(int m, int n)
+{   
+    int summa = n;
+    if(n < m) return 0;
+    return summa += GetSumma(m, n - 1);
+    
+}
+const int m = 1;
+const int n = 200;
+Console.WriteLine(GetSumma(m, n));
