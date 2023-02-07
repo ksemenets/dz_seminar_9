35737c50472bd128d75c25f2a37c4076997a632d
Konstantin <ksemenec@mail.ru>
2023-02-07 14:21:38 +0500

    task64 исправленная

--- task_64/Program.cs
+++ task_64/Program.cs
@@ -1,15 +1,15 @@
-﻿// Задача 64: Задайте значения M и N. Напишите программу,
-// которая выведет все натуральные числа в промежутке от M до N.
-// M = 1; N = 5. -> "1, 2, 3, 4, 5"
-// M = 4; N = 8. -> "4, 6, 7, 8"
+﻿// Задача 64: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 
-void PrintNaturalNumber(int n, int m)
-{
-    if(n < m) return;
-    PrintNaturalNumber(n - 1, m);
-    Console.Write ($"{n}, ");
+void PrintNaturalNumber(int n, int count)
+{    
+    if(count > n) return;
+    PrintNaturalNumber(n, count + 1);
+    Console.Write ($"{count}, ");
 }
-int m = 5;
-int n = 15;
-PrintNaturalNumber(n, m);
+int count = 1;
+int n = 43;
+PrintNaturalNumber(n, count);
